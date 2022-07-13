5eb0a12c42b0d785f4938e2b623d3ac16687ad39
Muffle10 <64377314+Muffle10@users.noreply.github.com>
2022-07-12 23:26:24 -0700

    Update main.py
    Made specifications and explained code more

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 arr = [1,10, 2, 1, 3, 4, 6, 4, 1, 4, 1, 2, 8, 9, 5, 6 ,9, 5, 5, 7, 5, 3]
+# A random array I made
 inp = 10
+# input - the number we need to find
 found = ""
+# Declaring variable called found globally
 print("Array being searched:")
 print(arr)
 while len(arr) > 1:
@@ -16,5 +19,9 @@ while len(arr) > 1:
         found = False
         break
     print(arr)
+# Sorting Algorithm
+
+
 if found:
     print(arr[0])
+ # Prints out found number in array;
